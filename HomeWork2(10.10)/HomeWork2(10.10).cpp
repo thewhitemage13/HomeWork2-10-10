473ffb89fc808a99b142a7ea3fa8c455833fd113
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-10-08 12:50:27 +0200

    Add files via upload

---
+++ HomeWork2(10.10)/HomeWork2(10.10).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+{
+	static int b=5;
+	static int a=1;
+
+	if (b < a)
+	{
+		cout << b;
+		b++;
+		if (b != a)
+		{
+			main();
+		}
+	}
+
+	else if (b > a)
+	{
+		b--;
+		cout << b;
+		if (a < b)
+		{
+			main();
+		}	
+	}
+}
